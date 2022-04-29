ba1ba44c74ff2cb91a1f96c1259845f551c45134
xhan <xin_hero111@163.com>
2022-04-29 18:22:41 +0800

    feat:echart

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,30 +1,35 @@
 <template>
   <el-menu
-    default-active="/index"
+    :default-active="$route.path"
     class="el-menu-vertical-demo"
     @open="handleOpen"
     @close="handleClose"
     router
     >
     <el-menu-item index="/index">
-        <el-icon><icon-menu /></el-icon>
+        <el-icon><Document/></el-icon>
         <span>首页</span>
     </el-menu-item>
     <el-menu-item index="/census">
-        <el-icon><icon-menu /></el-icon>
+        <el-icon><Location/></el-icon>
         <span>统计管理</span>
     </el-menu-item>
     <el-menu-item index="/order">
-        <el-icon><document /></el-icon>
+        <el-icon><Setting/></el-icon>
         <span>订单管理</span>
     </el-menu-item>
     </el-menu>
 </template>
 
 <script lang="ts" setup>
-function handleOpen(key: string, keyPath: string[]){}
-function handleClose(key: string, keyPath: string[]){}
-
+import {
+  Document,
+  Menu as IconMenu,
+  Location,
+  Setting,
+} from '@element-plus/icons-vue'
+const  handleOpen = (key: string, keyPath: string[]) => {};
+const  handleClose = (key: string, keyPath: string[]) => {};
 </script>
 
 <style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,11 +1,44 @@
 <template>
-  <div class="index">首页</div>
+  <div class="index">
+    <div ref="chart" style="width:100%;height:100%"></div>
+  </div>
+
 </template>
 
 <script lang="ts" setup>
+import * as echarts  from 'echarts'
+import { onMounted, ref } from 'vue-demi'
+const chart = ref();
+onMounted(()=>{
+  init();
+})
+const init = () => {
+  console.log('输出初始化数据')
+   var myChart = echarts.init(chart.value);
 
+      // 指定图表的配置项和数据
+      var option = {
+        tooltip: {},
+        legend: {
+          data: ['销量']
+        },
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [
+          {
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 100, 10, 10, 20]
+          }
+        ]
+      };
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
+}
 </script>
 
 <style>
 
-</style>
+</style> 
